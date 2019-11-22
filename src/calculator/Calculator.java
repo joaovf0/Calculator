f9c83474322f66148d78dd371f45db96f3c50135
Joao Gualberto Figueiredo (Retail) <JO40044425@wipro.com>
2019-11-22 13:19:19 -0300

    commit inicial

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author JO40044425
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+             
+        System.out.println("Digite um numero");
+        float num1 = input.nextFloat();
+        
+        System.out.println("Digite um numero");
+        float num2 = input.nextFloat();
+        
+        System.out.println("Digite um operador");
+        String oper = input.next();
+       
+        if(oper == "+"){
+            System.out.println("Resultado = "+ (num1+num2));
+        }
+    }
+    
+}
